f6a6043c2613f27a274f9752fa31d80fd7acc8b1
j-kerin <145474148+j-kerin@users.noreply.github.com>
2025-05-08 11:59:14 -0500

    Update Milestone_engagement.sql

--- Milestone_engagement.sql
+++ Milestone_engagement.sql
@@ -8,7 +8,7 @@ create temporary table  numbers AS (
     UNION ALL SELECT 5
     UNION ALL SELECT 6
     -- can add more than 6 weeks here if wanted
-)
+);
 
 create temporary table provider_weeks AS (
     SELECT 
@@ -21,7 +21,7 @@ create temporary table provider_weeks AS (
     WHERE 
         -- p.provider_id = 2337617  -- Replace with your specific provider_id 
     	p.signup_dt = '2025-03-01'-- between '2025-03-15' and '2025-04-01' -- replace with what signup dates your looking for
-)
+);
 
 
 create temporary table provider_weekly as (
@@ -32,7 +32,7 @@ SELECT
     DATEADD(day, 6, base_date) AS week_end_date
 FROM 
     provider_weeks
-)
+);
 
 create temp table weekly_rev as (
 select pw.provider_id, pw.week_number ,count(distinct adf.appointment_id ) apts, sum(pro_net_revenue) net_rev , sum(date_diff('minute', adf.start, adf.end ))apt_min -- * , date_diff('minute', adf.start, adf.end ) apt_min
@@ -41,7 +41,7 @@ join appointments_details_fact adf on  adf.provider_id = pw.provider_id
 	and adf.start between pw.week_start_date and pw.week_end_date 
 	and appt_state = 'completed'   -- and booking_type in ('client_user_booking','guest_user_booking')
 group by 1,2
-)
+);
 
 create temp table weekly_mp as (
 select pw.provider_id, pw.week_number
@@ -53,12 +53,12 @@ join provider_weekly pw on pw.provider_id = coalesce(sem.provider_id_viewed,sem.
 where sem.event_date >= '2025-02-01'::date
 	and sem.event in ('client_proprofile_viewed','client_appointment_booked') 
 group by 1,2
-)
+);
 create temp table weekly_aau as (
 	select pw.provider_id , pw.week_number, unique_visit_days , app_active_user_flag 
 	from analytics.app_active_users_daily_change aau
 	join provider_weekly pw on pw.provider_id = aau.provider_id and pw.week_end_date = aau.date
-)
+);
 
 select pw.*, wr.apts, wr.net_rev,  wr.apt_min, round((wr.net_rev /wr.apt_min *60) ,2)as hourly_rate
 	, mp.profile_views, mp.appointments_booked SS_apt_booked , aau.unique_visit_days, aau.app_active_user_flag as is_app_active_user
